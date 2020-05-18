9d3611fab7cfd04eafdcdeb227f26a78e0bbe748
dansem16 <56829993+dansem16@users.noreply.github.com>
2020-05-18 13:25:47 +0200

    Add files via upload

---
+++ Corona Danko/ue04_serienschaltung/src/ue04_collections/TestCollecion.java
@@ -0,0 +1,24 @@
+
+package ue04_collections;
+
+
+/**
+ *
+ * @author danko
+ */
+public class TestCollecion {
+
+    public TestCollecion () {
+        double x1 = 3.14;
+        Double x2 = 3.14;
+        
+    }
+    
+    
+    public static void main (String[] args) {
+      new TestCollecion();   
+    }
+
+
+    
+}
