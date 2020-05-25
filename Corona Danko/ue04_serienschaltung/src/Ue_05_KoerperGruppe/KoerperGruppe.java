5201a737ab80a6ec874ba8c740e2cc3ea5a4c2b0
dansem16 <56829993+dansem16@users.noreply.github.com>
2020-05-25 13:21:15 +0200

    Add files via upload

---
+++ Corona Danko/ue04_serienschaltung/src/Ue_05_KoerperGruppe/KoerperGruppe.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ue_05_KoerperGruppe;
+
+import java.util.List;
+
+
+/**
+ *
+ * @author danko
+ */
+class KoerperGruppe {
+
+    private boolean k;
+
+    
+
+    public KoerperGruppe () {
+        
+        private final List KoerperListe = List <Koerper>;
+        private double dichte;
+        
+        public KoerperGruppe( list KoerperListe)
+            
+       
+        public contains(boolean k : Koerper){
+        this.k = k;
+        
+            
+        }
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
---
+++ Corona Danko/ue04_serienschaltung/src/Ue_05_KoerperGruppe/TestKoerperGruppe.java
@@ -0,0 +1,32 @@
+
+package Ue_05_KoerperGruppe;
+
+
+/**
+ *
+ * @author danko
+ */
+public class TestKoerperGruppe {
+
+    public TestKoerperGruppe () {
+        KoerperGruppe g = new KoerperGruppe();
+        g.setDichte(Koerper.DICHTE_BUCHE);
+        
+        
+      g.add(new Wuerfel(0.1));  
+      g.add(new Wuerfel(0.05));
+      g.add(new Quadar(0.1, 0.1, 0.2, Koerper.DICHTE_BUCHE));
+      g.add(new Quadar(0.05, 0.1, 0.1));
+      g.add(new Kugel(0.1));
+      g.add(new kugel(0.15));
+      
+      
+      System.out.println("Koerpergruppe Besteht aus " + g.size() + "Körpern");
+      System.out.println(g);
+        
+      System.out.println("");
+      System.out.println(String.format("Gesamtmasse: %.3fkg", g.gesamtMasse()));
+      System.out.println(String.format("Gesamtvolumen: %.3fdm³", g.gesamtVolumen() * 1000));
+        
+    } 
+}
